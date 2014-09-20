b8e25e2c4e33377d857d6f1a8bee7b6dee0ed4c6
Anton Karpus <akarpus2@gmail.com>
2014-09-20 08:27:06 -0400

    Create Playlists.cs

---
+++ Classes/Playlists.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongSync
+{
+    class Playlists
+    {
+        public int user_ID { get; set; }
+        public string name { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
